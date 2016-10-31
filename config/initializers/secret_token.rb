6f3f9dc445c6a0885e59a56d2b23c29a9a0d6211
Mason Lantay <lantay@alumni.unc.edu>
2016-10-31 16:08:38 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixtopiathree::Application.config.secret_key_base = 'ad0b00f26599fec1c7ccd4ee250f4d7d34b23b6702614546c25e4356770f2a5b866ff6883cb10ee37aea703c19279966a46c9ca0b5c1b6da79a4b67041b1beb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flixtopiathree::Application.config.session_store :cookie_store, key: '_flixtopiathree_session'
